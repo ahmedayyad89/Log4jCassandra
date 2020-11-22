569a5ce5f0c6f3dfa0ffcaa590be3d064400eddf
ahmedayyad89 <ahmedayyad1410@gmail.com>
2020-11-22 10:52:38 +0200

    adding the project

---
+++ src/main/java/com/log/test/log4jcassandraappendertest/Log4jcassandraappendertestApplication.java
@@ -0,0 +1,13 @@
+package com.log.test.log4jcassandraappendertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Log4jcassandraappendertestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Log4jcassandraappendertestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/log/test/log4jcassandraappendertest/Log4jcassandraappendertestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.log.test.log4jcassandraappendertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class Log4jcassandraappendertestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
